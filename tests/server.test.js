46459ce8f5c5eeda09456cc8253b6757d3ccb646
emiliacoleto <emiliacoleto@gmail.com>
2021-12-04 19:36:19 +0100

    Tests config and trying to test something

---
+++ tests/server.test.js
@@ -0,0 +1,99 @@
+const app = require("../server.js");
+const controller = require("../src/controllers/ratings.js");
+const request = require("supertest");
+const Rating = require("../src/models/rating.js");
+
+describe("Ratings API", () => {
+  describe("GET /", () => {
+    it("should return an HTML document", () => {
+      return request(app)
+        .get("/")
+        .then((response) => {
+          expect(response.status).toBe(200);
+          expect(response.type).toEqual(expect.stringContaining("html"));
+          expect(response.text).toEqual(expect.stringContaining("h1"));
+        });
+    });
+  });
+
+  /* describe("GET /ratings", () => {
+    beforeAll(() => {
+      const ratings = [
+        {
+          "value": "4.5",
+          "description": "Good film",
+          "film": "1",
+          "user": "11",
+          "date": "2020-12-02T23:00:00.000+00:00",
+        },
+        {
+          "value": "1.5",
+          "description": "Bad film",
+          "film": "15",
+          "user": "19",
+          "date": "2021-12-02T23:00:00.000+00:00",
+        },
+      ];
+
+      dbFind = jest.spyOn(Rating, "find");
+      dbFind.mockImplementation((query, callback) => {
+        callback(null, ratings);
+      });
+    });
+
+    it("should return all ratings", () => {
+      return request(app)
+        .get("/api/v1/ratings")
+        .then((response) => {
+          jest.setTimeout(200)
+          console.log(response)
+          expect(response.statusCode).toBe(200);
+          expect(response.body).toBeArrayOfSize(2);
+          expect(dbFind).toBeCalledWith({}, expect.any(Function))
+        });
+    });
+  });
+
+  describe("POST /ratings", () => {
+    const rating = {
+      value: 4.5,
+      description: "Good film",
+      film: 1,
+      user: 11,
+      date: "2020-12-02T23:00:00.000+00:00",
+    };
+
+    let dbInsert;
+
+    beforeEach(() => {
+      dbInsert = jest.spyOn(Rating, "insert");
+    });
+
+    it("should add a new rating", () => {
+      dbInsert.mockImplementation((r, callback) => {
+        callback(false);
+      });
+
+      return request(app)
+        .post("/api/v1/ratings")
+        .send(rating)
+        .then((response) => {
+          expect(response.statusCode).toBe(201);
+          expect(dbInsert).toBeCalledWith(rating, expect.any(Function));
+        });
+    });
+
+    it("should return 500 if there is a problem with the DB", () => {
+      dbInsert.mockImplementation((r, callback) => {
+        callback(true);
+      });
+
+      return request(app)
+        .post("/api/v1/ratings")
+        .send(rating)
+        .then((response) => {
+          expect(response.statusCode).toBe(500);
+        });
+    });
+  }); */
+});
